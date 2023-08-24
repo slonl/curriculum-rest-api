9fbe94fc38dfc828b3fda02d430b7030b25e4545
govert@muze.nl <govert@muze.nl>
2023-08-24 17:21:51 +0200

    curriculum-inhoudslijnen.js WIP

--- src/opendata-api.js
+++ src/opendata-api.js
@@ -17,6 +17,7 @@ var apis = [
 	require('./opendata-api/curriculum-erk.js'),
 	require('./opendata-api/curriculum-examenprogramma.js'),
 	require('./opendata-api/curriculum-examenprogramma-bg.js'),
+	require('./opendata-api/curriculum-inhoudslijnen.js'),
 	require('./opendata-api/curriculum-kerndoelen.js'),
 	require('./opendata-api/curriculum-leerdoelenkaarten.js'),
 	require('./opendata-api/curriculum-referentiekader.js'),
