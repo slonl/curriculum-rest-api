d82a89391fdb87fdab4c65c6308012a198ac2acf
Auke van Slooten <auke@muze.nl>
2023-09-14 14:35:31 +0200

    adds @id and @type keys if no niveaus or contexts are given, return all

--- src/tree-query.js
+++ src/tree-query.js
@@ -1,10 +1,10 @@
 function addToKey(data, key, value) {
-	if (data[key]) {
-		if (!Array.isArray(data[key])) {
-			data[key] = [ data[key] ]
-		}
-		data[key].push(value)
-	} else {
+    if (data[key]) {
+        if (!Array.isArray(data[key])) {
+            data[key] = [ data[key] ]
+        }
+        data[key].push(value)
+    } else {
         data[key] = value;
     }
 }
@@ -26,63 +26,71 @@ function flattenObj(ob) {
         if (isObject(ob[i])) {
             const temp = flattenObj(ob[i]);
             for (const j in temp) {
-            	addToKey(result, j, temp[j])
+                addToKey(result, j, temp[j])
             }
         } else {
-        	addToKey(result, i, ob[i])
+            addToKey(result, i, ob[i])
         }
     }
     return result;
 };
 
 function inNiveaus(e, niveaus) {
-	if (!Array.isArray(niveaus)) {
-		niveaus = [niveaus]
-	}
-	let hasNiveaus = e.Niveau 
-	? from(e.Niveau).select({title: _}) 
-	: from(e.NiveauIndex).select({Niveau: {title: _}})
-	hasNiveaus = flattenObj(hasNiveaus)
+    if (!Array.isArray(niveaus)) {
+        niveaus = [niveaus]
+    }
+    let hasNiveaus = e.Niveau 
+    ? from(e.Niveau).select({title: _}) 
+    : from(e.NiveauIndex).select({Niveau: {title: _}})
+    hasNiveaus = flattenObj(hasNiveaus)
     return niveaus.filter(value => {
       return hasNiveaus?.title?.includes(value)
     }).length>0
 }
 
 function getAll(contexts, niveaus) {
-	return (data) => {
-		let result = {}
-		Object.entries(data).forEach(([k,v]) => {
-			if (Array.isArray(v)) {
-				result[k] = v.map(e => {
-					if (isObject(e)) {
-						if (inContexts(e, contexts) && inNiveaus(e, niveaus)) {
+    return (data) => {
+        let result = {}
+        Object.entries(data).forEach(([k,v]) => {
+            if (Array.isArray(v)) {
+                result[k] = v.map(e => {
+                    if (isObject(e)) {
+                        if ((!contexts || !contexts.length || inContexts(e, contexts)) 
+                            && (!niveaus || !niveaus.length || inNiveaus(e, niveaus))) {
                             let f = getAll(contexts,niveaus)
-							return f(e)
-						}
-						// else return null, which will be filtered
-					} else {
-						return e
-					}
-				}).filter(Boolean)
-			} else {
-				result[k] = v
-			}
-		})
+                            return f(e)
+                        }
+                        // else return null, which will be filtered
+                    } else {
+                        return e
+                    }
+                }).filter(Boolean)
+            } else {
+                if (k==='id') {
+                    result['@id'] = 'https://opendata.slo.nl/curriculum/uuid/'+v
+                    k = 'uuid'
+                }
+                result[k] = v
+            }
+        })
+        if (result.uuid) {
+            result['@type'] = JSONTag.getAttribute(data, 'class')
+        }
         return result
-	}
+    }
 }
 
 function getContextByType(type) {
-	let contexts = {"NhCategorie":"curriculum-niveauhierarchie","NhSector":"curriculum-niveauhierarchie","NhSchoolsoort":"curriculum-niveauhierarchie","NhLeerweg":"curriculum-niveauhierarchie","NhBouw":"curriculum-niveauhierarchie","NhNiveau":"curriculum-niveauhierarchie","Deprecated":"curriculum-samenhang","LdkVakleergebied":"curriculum-leerdoelenkaarten","LdkVakkern":"curriculum-leerdoelenkaarten","LdkVaksubkern":"curriculum-leerdoelenkaarten","LdkVakinhoud":"curriculum-leerdoelenkaarten","LdkVakbegrip":"curriculum-leerdoelenkaarten","SyllabusVakleergebied":"curriculum-syllabus","Syllabus":"curriculum-syllabus","SyllabusSpecifiekeEindterm":"curriculum-syllabus","SyllabusToelichting":"curriculum-syllabus","SyllabusVakbegrip":"curriculum-syllabus","ExamenprogrammaBody":"curriculum-examenprogramma","ExamenprogrammaKop4":"curriculum-examenprogramma","ExamenprogrammaKop3":"curriculum-examenprogramma","ExamenprogrammaKop2":"curriculum-examenprogramma","ExamenprogrammaKop1":"curriculum-examenprogramma","ExamenprogrammaEindterm":"curriculum-examenprogramma","ExamenprogrammaSubdomein":"curriculum-examenprogramma","ExamenprogrammaDomein":"curriculum-examenprogramma","ExamenprogrammaVakleergebied":"curriculum-examenprogramma","Examenprogramma":"curriculum-examenprogramma","ErkVakleergebied":"curriculum-erk","ErkGebied":"curriculum-erk","ErkCategorie":"curriculum-erk","ErkTaalactiviteit":"curriculum-erk","ErkSchaal":"curriculum-erk","ErkCandobeschrijving":"curriculum-erk","ErkVoorbeeld":"curriculum-erk","ErkLesidee":"curriculum-erk","InhVakleergebied":"curriculum-inhoudslijnen","InhInhoudslijn":"curriculum-inhoudslijnen","InhCluster":"curriculum-inhoudslijnen","InhSubcluster":"curriculum-inhoudslijnen","Kerndoel":"curriculum-kerndoelen","KerndoelVakleergebied":"curriculum-kerndoelen","KerndoelDomein":"curriculum-kerndoelen","KerndoelUitstroomprofiel":"curriculum-kerndoelen","Leerlingtekst":"curriculum-doelgroepteksten","FoDomein":"curriculum-fo","FoSubdomein":"curriculum-fo","FoDoelzin":"curriculum-fo","FoUitwerking":"curriculum-fo","FoToelichting":"curriculum-fo","Doelniveau":"curriculum-basis","Doel":"curriculum-basis","Niveau":"curriculum-basis","Vakleergebied":"curriculum-basis","Alias":"curriculum-basis","ExamenprogrammaBgProfiel":"curriculum-examenprogramma-bg","ExamenprogrammaBgKern":"curriculum-examenprogramma-bg","ExamenprogrammaBgKerndeel":"curriculum-examenprogramma-bg","ExamenprogrammaBgModuletaak":"curriculum-examenprogramma-bg","ExamenprogrammaBgKeuzevaktaak":"curriculum-examenprogramma-bg","ExamenprogrammaBgModule":"curriculum-examenprogramma-bg","ExamenprogrammaBgKeuzevak":"curriculum-examenprogramma-bg","ExamenprogrammaBgDeeltaak":"curriculum-examenprogramma-bg","ExamenprogrammaBgGlobaleEindterm":"curriculum-examenprogramma-bg","Tag":"curriculum-samenhang","Relatie":"curriculum-samenhang"}
-	return contexts[type]
+    let contexts = {"NhCategorie":"curriculum-niveauhierarchie","NhSector":"curriculum-niveauhierarchie","NhSchoolsoort":"curriculum-niveauhierarchie","NhLeerweg":"curriculum-niveauhierarchie","NhBouw":"curriculum-niveauhierarchie","NhNiveau":"curriculum-niveauhierarchie","Deprecated":"curriculum-samenhang","LdkVakleergebied":"curriculum-leerdoelenkaarten","LdkVakkern":"curriculum-leerdoelenkaarten","LdkVaksubkern":"curriculum-leerdoelenkaarten","LdkVakinhoud":"curriculum-leerdoelenkaarten","LdkVakbegrip":"curriculum-leerdoelenkaarten","SyllabusVakleergebied":"curriculum-syllabus","Syllabus":"curriculum-syllabus","SyllabusSpecifiekeEindterm":"curriculum-syllabus","SyllabusToelichting":"curriculum-syllabus","SyllabusVakbegrip":"curriculum-syllabus","ExamenprogrammaBody":"curriculum-examenprogramma","ExamenprogrammaKop4":"curriculum-examenprogramma","ExamenprogrammaKop3":"curriculum-examenprogramma","ExamenprogrammaKop2":"curriculum-examenprogramma","ExamenprogrammaKop1":"curriculum-examenprogramma","ExamenprogrammaEindterm":"curriculum-examenprogramma","ExamenprogrammaSubdomein":"curriculum-examenprogramma","ExamenprogrammaDomein":"curriculum-examenprogramma","ExamenprogrammaVakleergebied":"curriculum-examenprogramma","Examenprogramma":"curriculum-examenprogramma","ErkVakleergebied":"curriculum-erk","ErkGebied":"curriculum-erk","ErkCategorie":"curriculum-erk","ErkTaalactiviteit":"curriculum-erk","ErkSchaal":"curriculum-erk","ErkCandobeschrijving":"curriculum-erk","ErkVoorbeeld":"curriculum-erk","ErkLesidee":"curriculum-erk","InhVakleergebied":"curriculum-inhoudslijnen","InhInhoudslijn":"curriculum-inhoudslijnen","InhCluster":"curriculum-inhoudslijnen","InhSubcluster":"curriculum-inhoudslijnen","Kerndoel":"curriculum-kerndoelen","KerndoelVakleergebied":"curriculum-kerndoelen","KerndoelDomein":"curriculum-kerndoelen","KerndoelUitstroomprofiel":"curriculum-kerndoelen","Leerlingtekst":"curriculum-doelgroepteksten","FoDomein":"curriculum-fo","FoSubdomein":"curriculum-fo","FoDoelzin":"curriculum-fo","FoUitwerking":"curriculum-fo","FoToelichting":"curriculum-fo","Doelniveau":"curriculum-basis","Doel":"curriculum-basis","Niveau":"curriculum-basis","Vakleergebied":"curriculum-basis","Alias":"curriculum-basis","ExamenprogrammaBgProfiel":"curriculum-examenprogramma-bg","ExamenprogrammaBgKern":"curriculum-examenprogramma-bg","ExamenprogrammaBgKerndeel":"curriculum-examenprogramma-bg","ExamenprogrammaBgModuletaak":"curriculum-examenprogramma-bg","ExamenprogrammaBgKeuzevaktaak":"curriculum-examenprogramma-bg","ExamenprogrammaBgModule":"curriculum-examenprogramma-bg","ExamenprogrammaBgKeuzevak":"curriculum-examenprogramma-bg","ExamenprogrammaBgDeeltaak":"curriculum-examenprogramma-bg","ExamenprogrammaBgGlobaleEindterm":"curriculum-examenprogramma-bg","Tag":"curriculum-samenhang","Relatie":"curriculum-samenhang"}
+    return contexts[type]
 }
 
 function inContexts(e, contexts) {
-	if (!Array.isArray(contexts)) {
-		contexts = [contexts]
-	}
-	let className = JSONTag.getAttribute(e, 'class')
-	let context = getContextByType(className)
+    if (!Array.isArray(contexts)) {
+        contexts = [contexts]
+    }
+    let className = JSONTag.getAttribute(e, 'class')
+    let context = getContextByType(className)
     return contexts.indexOf(context)!==-1; //s.some(context)
 }
 
