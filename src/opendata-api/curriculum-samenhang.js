e0f8e8782e99ea7f82b45197fdd00cfa6142268b
Jurre Koning <jurre@vanced.nl>
2023-09-26 15:30:31 +0200

    branch voor beter tonen kerndoelen

--- src/opendata-api/curriculum-samenhang.js
+++ src/opendata-api/curriculum-samenhang.js
@@ -1,50 +1,110 @@
 module.exports = {
-	context: 'Samenhang',
-	jsonld: 'https://opendata.slo.nl/curriculum/schemas/samenhang.jsonld',
-	schema: 'https://opendata.slo.nl/curriculum/schemas/curriculum-samenhang/context.json',
-	queries: {
-		Tag: `query Tag($page:Int, $perPage: Int) {
-			allTag(page:$page, perPage:$perPage, sortField:"title", filter:{deprecated:null}) {
-				id
-				title
-				unreleased
-			}
-			_allTagMeta {
-				count
-			}
-		} 
-		`
-	},
-	typedQueries: {
-		'tag': `
-			id
-			title
-			FoDoelzin {
-				id
-				title
-				deprecated
-			}
-			FoToelichting {
-				id
-				title
-				deprecated
-			}
-			FoUitwerking {
-				id
-				title
-				deprecated
-			}
-		`
-	},
-	routes: {
-		'tag/': (req) =>
-			opendata.api["Tag"](req.params, req.query)
-			.then(function(result) {
-				return { 
-					data: result.data.allTag, 
-					type: 'Tag', 
-					meta: result.data._allTagMeta
-				}
-			}),
-	}
+        context: 'samenhang',
+        jsonld: 'https://opendata.slo.nl/curriculum/schemas/samenhang.jsonld',
+        schema: 'https://opendata.slo.nl/curriculum/schemas/curriculum-samenhang/context.json',
+        queries: {
+                Tag: `query Tag($page:Int, $perPage:Int) {
+                        allTag(page:$page, perPage:$perPage, filter:{deprecated:null}) {
+                                id
+                                title
+                                unreleased
+                                fo_toelichting_id
+                                fo_uitwerking_id
+                        }
+                        _allTagMeta {
+                                count
+                        }
+                }`,
+                Relatie: `query Relatie($page:Int, $perPage:Int) {
+                        allRelatie(page:$page, perPage:$perPage, filter:{deprecated:null}) {
+                                id
+                                title
+                                unreleased
+                                fo_toelichting_id
+                                fo_uitwerking_id
+                        }
+                        _allRelatieMeta {
+                                count
+                        }
+                }`
+        },
+        typedQueries: {
+                'tag': `
+                        id
+                        title
+                        FoToelichting {
+                                id
+                                title
+                                deprecated
+                        }
+                        FoUitwerking {
+                                id
+                                title
+                                deprecated
+                        }
+                `,
+                'relatie': `
+                        id
+                        title
+                        FoToelichting {
+                                id
+                                title
+                                deprecated
+                        }
+                        FoUitwerking {
+                                id
+                                title
+                                deprecated
+                        }
+                `
+        },
+        idQuery: `
+                allTag(filter:{id:$id}) {
+                        id
+                        title
+                        FoToelichting {
+                                id
+                                title
+                                deprecated
+                        }
+                        FoUitwerking {
+                                id
+                                title
+                                deprecated
+                        }
+                }
+                allRelatie(filter:{id:$id}) {
+                        id
+                        title
+                        FoToelichting {
+                                id
+                                title
+                                deprecated
+                        }
+                        FoUitwerking {
+                                id
+                                title
+                                deprecated
+                        }
+                }`,
+        routes: {
+                'tag/': (req) =>
+                        opendata.api["Tag"](req.params, req.query)
+                        .then(function(result) {
+                                return {
+                                        data: result.data.allTag,
+                                        type: 'Tag',
+                                        meta: result.data._allTagMeta
+                                }
+                        }),
+                'relatie/': (req) =>
+                        opendata.api["Relatie"](req.params, req.query)
+                        .then(function(result) {
+                                return {
+                                        data: result.data.allRelatie,
+                                        type: 'Relatie',
+                                        meta: result.data._allRelatieMeta
+                                }
+                        })
+        }
 };
