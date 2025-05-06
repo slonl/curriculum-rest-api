e4534b024b39e5dd769550af7871f346a6424e0d
govert@muze.nl <govert@muze.nl>
2025-05-06 15:43:58 +0200

    javascript is case sensitive when it comes to 'numbers' and 'booleans', or so claude tells me.

--- src/api-server.js
+++ src/api-server.js
@@ -234,7 +234,7 @@ function addReference(entry){
 }
 
 function isObject(value){
-	if (value !== null && typeof value == 'object' && typeof value !== 'string' && typeof value !== 'Number' && typeof value !== "Boolean"){
+	if (value !== null && typeof value == 'object' && typeof value !== 'string' && typeof value !== 'number' && typeof value !== "boolean"){
 		return true;
 	}
 	else {
