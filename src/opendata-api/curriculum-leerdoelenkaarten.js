3e4ac6cdd2667d2ae1052ad716a6faa574b1d00c
govert@muze.nl <govert@muze.nl>
2025-05-19 11:11:09 +0200

    niveau moest in de typedQueries, bij deze

--- src/opendata-api/curriculum-leerdoelenkaarten.js
+++ src/opendata-api/curriculum-leerdoelenkaarten.js
@@ -210,6 +210,7 @@ module.exports = {
 			}
 		})`,
 		LdkVakleergebiedByIdOpNiveau: `
+		console.log("LdkVakleergebiedByIdOpNiveau called")
 		const results = from(data.NiveauIndex)
 		.where({
 			uuid: request.query.id
@@ -232,19 +233,6 @@ module.exports = {
 			},
 			Niveau: NiveauShort
 		})
-			
-		results.Niveau
-				.sort((a,b) => a.prefix<b.prefix ? -1 : 1)
-				.map(child => {
-					child['$ref'] =   request.query.baseDatasetURL + "niveau/";+ (child.uuid ?? child.id);
-					if (entry['@type']=='Vakleergebied') {
-						child['$ref'] += '/vakleergebied/' + (entry.uuid ?? entry.id);
-					}
-					return child;
-				});
-			
-		results
-
 		`,
 
 		LdkVakkernOpNiveau: `
@@ -346,7 +334,7 @@ module.exports = {
 	},
 	typedQueries: {
 		LdkVakleergebied: `
-		from(Index(request.query.id))
+		const results = from(Index(request.query.id))
 		.select({
 			...shortInfo,
 			LdkVakkern: {
@@ -361,6 +349,19 @@ module.exports = {
 				deprecated: _,
 			}
 		})
+
+		results.Niveau
+		.sort((a,b) => a.prefix<b.prefix ? -1 : 1)
+		.map(child => {
+			child['$ref'] =   request.query.baseDatasetURL + "niveau/";+ (child.uuid ?? child.id);
+			if (entry['@type']=='Vakleergebied') {
+				child['$ref'] += '/vakleergebied/' + (entry.uuid ?? entry.id);
+			}
+			return child;
+		});
+			
+		results
+
 		`,
 		LdkVakkern: `
 		from(Index(request.query.id))
